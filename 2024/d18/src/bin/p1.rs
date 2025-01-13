32b809a5832b6b2b9e5825ac0cad40b0c0e66a93
simon mares <simon.mares@gmail.com>
2025-01-13 16:20:49 +0100

    d18 ok

--- 2024/d18/src/bin/p1.rs
+++ 2024/d18/src/bin/p1.rs
@@ -4,11 +4,7 @@ use d18::solve_p1;
 
 fn main() {
     let n = Instant::now();
-    let res = solve_p1(std::fs::read_to_string("in.txt").unwrap());
+    let res = solve_p1(std::fs::read_to_string("in.txt").unwrap(), 1024);
     let needed = Instant::now() - n;
-    println!(
-        "res: {:?}     ... needed {}",
-        res,
-        needed.as_secs_f32()
-    );
+    println!("res: {:?}     ... needed {}", res, needed.as_secs_f32());
 }
--- 2024/d18/src/lib.rs
+++ 2024/d18/src/lib.rs
@@ -14,32 +14,33 @@ use std::{f32::INFINITY, sync::Arc};
 use miette::miette;
 
 fn parse(s: &str) -> IResult<&str, Vec<(i32, i32)>> {
-    let r = separated_list0(
-        tag("\n"),
-        separated_pair(digit0, tag(","), digit0),
-    )(s)?;
-    let res = r
-        .1
-        .into_iter()
-        .map(|i| {
-            (i.0.parse::<i32>().unwrap(), i.1.parse::<i32>().unwrap())
-        })
-        .collect();
+    let r = separated_list0(tag("\n"), separated_pair(digit0, tag(","), digit0))(s)?;
+    let res =
+        r.1.into_iter()
+            .map(|i| (i.0.parse::<i32>().unwrap(), i.1.parse::<i32>().unwrap()))
+            .collect();
     Ok((r.0, res))
 }
 
-pub fn solve_p1(contents: String) -> miette::Result<String> {
+pub fn solve_p1(contents: String, steps: usize) -> miette::Result<String> {
     println!("start solve p1");
 
-    let (_, corrupted) = parse(&contents[..])
-        .map_err(|e| miette!("could not parse, err: {}", e))?;
+    let (_, corrupted_all) =
+        parse(&contents[..]).map_err(|e| miette!("could not parse, err: {}", e))?;
 
-    let w = corrupted.iter().map(|x| x.0).max().unwrap() + 1;
-    let h = corrupted.iter().map(|x| x.1).max().unwrap() + 1;
+    let w = corrupted_all.iter().map(|x| x.0).max().unwrap() + 1;
+    let h = corrupted_all.iter().map(|x| x.1).max().unwrap() + 1;
 
-    print_map(w, h, &corrupted);
+    // print_map(w, h, &corrupted_all);
 
     let goal = (w - 1, h - 1);
+
+    // let mut ci = 0;
+    let mut corrupted = vec![];
+
+    for ii in 0..steps {
+        corrupted.push(corrupted_all[ii]);
+    }
 
     let result = dijkstra(
         &(0, 0),
@@ -57,19 +58,82 @@ pub fn solve_p1(contents: String) -> miette::Result<String> {
             if y < h - 1 && !corrupted.contains(&(x, y + 1)) {
                 res.push((x, y + 1));
             }
-            println!("currently at {:?}    opts: {:?}", (x, y), &res);
+            // println!("currently at {:?}    opts: {:?}", (x, y), &res);
+
+            // corrupted.push(corrupted_all[ci]);
+            // ci += 1;
+
             res.into_iter().map(|p| (p, 1))
         },
         |&p| p == goal,
     );
 
-    Ok(String::from("0"))
+    if let Some(res) = result {
+        Ok(res.1.to_string())
+    } else {
+        Ok(String::from("0"))
+    }
 }
 
 pub fn solve_p2(contents: String) -> miette::Result<String> {
     println!("start solve p2");
 
-    Ok(String::from("0"))
+    let (_, corrupted_all) =
+        parse(&contents[..]).map_err(|e| miette!("could not parse, err: {}", e))?;
+
+    let w = corrupted_all.iter().map(|x| x.0).max().unwrap() + 1;
+    let h = corrupted_all.iter().map(|x| x.1).max().unwrap() + 1;
+
+    // print_map(w, h, &corrupted_all);
+
+    let goal = (w - 1, h - 1);
+
+    // let mut ci = 0;
+    let mut corrupted = vec![];
+    let mut is_ok_max = 0;
+
+    for ii in 0..corrupted_all.len() {
+        println!("current step {}", ii);
+        corrupted.push(corrupted_all[ii]);
+
+        let result = dijkstra(
+            &(0, 0),
+            |&(x, y)| {
+                let mut res = vec![];
+                if x > 0 && !corrupted.contains(&(x - 1, y)) {
+                    res.push((x - 1, y));
+                }
+                if x < w - 1 && !corrupted.contains(&(x + 1, y)) {
+                    res.push((x + 1, y));
+                }
+                if y > 0 && !corrupted.contains(&(x, y - 1)) {
+                    res.push((x, y - 1));
+                }
+                if y < h - 1 && !corrupted.contains(&(x, y + 1)) {
+                    res.push((x, y + 1));
+                }
+                // println!("currently at {:?}    opts: {:?}", (x, y), &res);
+
+                // corrupted.push(corrupted_all[ci]);
+                // ci += 1;
+
+                res.into_iter().map(|p| (p, 1))
+            },
+            |&p| p == goal,
+        );
+
+        if result.is_some() {
+            is_ok_max = ii;
+        } else {
+            break;
+        }
+    }
+
+    Ok(format!(
+        "{},{}",
+        corrupted_all[is_ok_max + 1].0,
+        corrupted_all[is_ok_max + 1].1
+    ))
 }
 
 fn print_map(w: i32, h: i32, corrupted: &Vec<(i32, i32)>) {
@@ -91,7 +155,45 @@ mod tests {
 
     #[test]
     fn p1_1() -> miette::Result<()> {
-        let res = solve_p1(String::from(
+        let res = solve_p1(
+            String::from(
+                "5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0",
+            ),
+            12,
+        )?;
+
+        assert_eq!(res, "22");
+
+        Ok(())
+    }
+
+    #[test]
+    fn p2_1() -> miette::Result<()> {
+        let res = solve_p2(String::from(
             "5,4
 4,2
 4,5
@@ -119,21 +221,8 @@ mod tests {
 2,0",
         ))?;
 
-        assert_eq!(res, "22");
+        assert_eq!(res, "6,1");
 
         Ok(())
     }
-
-    //     #[test]
-    //     fn p2_1() -> miette::Result<()> {
-    //         let solution = solve_p1(String::from(
-    //             "Register A: 117440
-    // Register B: 0
-    // Register C: 0
-
-    // Program: 0,3,5,4,3,0",
-    //         ))?;
-
-    //         Ok(())
-    //     }
 }
